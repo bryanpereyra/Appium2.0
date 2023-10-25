612626dc7edb11e4ed95e00ca767197d44d6b909
Bryan Pereyra <49075258+bryanpereyra@users.noreply.github.com>
2023-10-25 13:42:49 -0600

    Add resources package

--- src/test/java/bryanpereyra/Appium/AppiumBasics.java
+++ src/test/java/bryanpereyra/Appium/AppiumBasics.java
@@ -1,5 +1,25 @@
 package bryanpereyra.Appium;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.testng.annotations.Test;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
 public class AppiumBasics {
+
+	@Test
+	public void AppiumTest() throws MalformedURLException {
+
+		// AndroidDriver object
+		// Appium Code -> Appium Server -> Mobile
+		UiAutomator2Options options = new UiAutomator2Options();
+		options.setDeviceName("Pixel 7 Pro");
+		
+		AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), null);
+
+	}
 
 }
